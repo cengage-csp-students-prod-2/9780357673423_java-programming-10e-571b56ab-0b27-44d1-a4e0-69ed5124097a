0298119e0c4ff9909d4b93fd48667151f7c9e2b5
Om Mahato <117744764+MahatoOm@users.noreply.github.com>
2025-03-26 15:46:32 +0000

    Testing Wed Mar 26 2025 15:46:32 GMT+0000 (Coordinated Universal Time)

--- chapter1/ex07/student/MovieQuoteInfo.java
+++ chapter1/ex07/student/MovieQuoteInfo.java
@@ -1,1 +1,12 @@
 // Write your code here
+class MovieQuoteInfo{
+    public static void main(String[] args){
+        System.out.println("Rosebud,");
+        System.out.println("said by Charles Foster Kane");
+        System.out.println("in the movie Citizen kane");
+        System.out.println("in 1941.");
+
+
+    }
+
+}
