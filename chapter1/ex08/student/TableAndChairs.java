bc16c95264e506a9a6e5427d3e547776d964dc25
Om Mahato <117744764+MahatoOm@users.noreply.github.com>
2025-04-02 15:00:45 +0000

    Testing Wed Apr 02 2025 15:00:44 GMT+0000 (Coordinated Universal Time)

--- chapter1/ex08/student/TableAndChairs.java
+++ chapter1/ex08/student/TableAndChairs.java
@@ -1,1 +1,8 @@
 // Write your code here
+
+X                      X
+X                      X
+X      XXXXXXXXXX      X
+XXXXX  X        X  XXXXX
+X   X  X        X  X   X
+X   X  X        X  X   X
--- chapter2/ex10/student/MinutesConversion.java
+++ chapter2/ex10/student/MinutesConversion.java
@@ -1,1 +1,25 @@
 // Write your code here
+
+
+import java.util.Scanner;
+public class MinutesConversion{
+public static void main(String[] args){
+
+    int minutes; double hours, days;
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter minutes >> ");
+
+    minutes = input.nextInt();
+
+    final double hours_in_day = 24;
+    final double minutes_in_day = 60;
+
+    hours = minutes / minutes_in_day;
+    days = hours / hours_in_day;
+
+    System.out.printf("%d minutes is %f hours or %f days.", minutes, hours , days);
+
+
+}
+
+}
