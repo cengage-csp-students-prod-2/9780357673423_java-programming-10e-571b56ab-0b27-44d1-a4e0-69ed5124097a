749790169d0874a69e4f00293efefb09a660d5bd
Om Mahato <117744764+MahatoOm@users.noreply.github.com>
2025-04-03 07:10:11 +0000

    Testing Thu Apr 03 2025 07:10:11 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex08/student/ChiliToGo.java
+++ chapter2/ex08/student/ChiliToGo.java
@@ -1,1 +1,29 @@
 // Write your code here
+// Write your code here
+import java.util.Scanner;
+public class ChillTOGo{
+
+    public static void main(String[] args){
+        double adult, child;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter number of adult meals ordered >> ");
+        adult = input.nextDouble();
+        System.out.print("Enter number of child meals ordered >> ");
+        child = input.nextDouble();
+
+        System.out.print(adult + "adult meals were ordered at $" + 7.0 + " each.");
+        System.out.print("Total is $" + adult * 7.0);
+
+        System.out.print(child + "child meals were ordered at $" + 4.0 + " each.");
+        System.out.print("Total is $" + child * 7.0);
+
+        System.out.print("Grnad total for all meals is $" + adult * child * 28.0);
+
+
+
+
+
+
+    }
+
+}
--- chapter2/ex08/student/ChiliToGoProfit.java
+++ chapter2/ex08/student/ChiliToGoProfit.java
@@ -1,1 +1,35 @@
 // Write your code here
+import java.util.Scanner;
+public class ChillTOGo{
+
+    public static void main(String[] args){
+        double adult, child;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter number of adult meals ordered >> ");
+        adult = input.nextDouble();
+        System.out.print("Enter number of child meals ordered >> ");
+        child = input.nextDouble();
+
+        System.out.print(adult + "adult meals were ordered at $" + 7.0 + " each.");
+        System.out.print("Total is $" + adult * 7.0);
+
+        System.out.print(child + "child meals were ordered at $" + 4.0 + " each.");
+        System.out.print("Total is $" + child * 7.0);
+
+        System.out.print("Grnad total for all meals is $" + adult * child * 28.0);
+
+
+        System.out.print("Profits:");
+        System.out.print("Adult profits is $" + adult * 2.65);
+        System.out.print("Child profits is $" + child * 0.90);
+
+        System.out.print("TOtal profits is $" + child * adult * 2.385);
+
+
+
+
+
+
+    }
+
+}
