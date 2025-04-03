66d3c64add5d51fb34c64907e9bcecaae000bb5d
Om Mahato <117744764+MahatoOm@users.noreply.github.com>
2025-04-03 07:35:14 +0000

    Testing Thu Apr 03 2025 07:35:13 GMT+0000 (Coordinated Universal Time)

--- chapter1/ex08/student/TableAndChairs.java
+++ chapter1/ex08/student/TableAndChairs.java
@@ -1,8 +1,18 @@
 // Write your code here
 
-X                      X
-X                      X
-X      XXXXXXXXXX      X
-XXXXX  X        X  XXXXX
-X   X  X        X  X   X
-X   X  X        X  X   X
+public class TableAndChair{
+    public static void main(String[] args){
+
+
+System.out.println("X                      X");
+System.out.println("X                      X");
+System.out.println("X      XXXXXXXXXX      X");
+System.out.println("XXXXX  X        X  XXXXX");
+System.out.println("X   X  X        X  X   X");
+System.out.println("X   X  X        X  X   X");
+
+
+
+}
+
+}
