66626071790d1285dbd155dc7961ec8b8634afa5
Om Mahato <117744764+MahatoOm@users.noreply.github.com>
2025-04-03 08:02:10 +0000

    Testing Thu Apr 03 2025 08:02:10 GMT+0000 (Coordinated Universal Time)

--- chapter1/ex10/student/Comments.java
+++ chapter1/ex10/student/Comments.java
@@ -19,11 +19,16 @@ public class Comments{
     }
 
     public static void main(String[] args){
+        // Program comments are nonexecuting statements 
+        //you add to a file for documentation 
+
 /* Program comments are nonexecuting statements 
 you add to a file for documentation 
 */
-//    System.out.println("Program comments are nonexecuting statements ");
-//    System.out.println("you add to a file for documentation");
+/**    System.out.println("Program comments are nonexecuting statements ");
+*  System.out.println("you add to a file for documentation");
+
+*/
         Comments();
     }
 }
