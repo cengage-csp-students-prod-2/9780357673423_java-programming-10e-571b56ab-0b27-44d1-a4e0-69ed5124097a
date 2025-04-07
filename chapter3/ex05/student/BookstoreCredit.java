43f7a599089451fc84ad555088cc7d3d7a4a7d32
Om Mahato <117744764+MahatoOm@users.noreply.github.com>
2025-04-07 15:30:58 +0000

    Testing Mon Apr 07 2025 15:30:58 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex05/student/BookstoreCredit.java
+++ chapter3/ex05/student/BookstoreCredit.java
@@ -1,1 +1,22 @@
 // Write your code here
+import java.util.Scanner;
+public class BookstoreCredit{
+
+    public static void  main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Your Name >> ");
+        String name = input.nextLine();
+
+        System.out.print("Enter Your Gpa >> ");
+        double gpa = input.nextDouble();
+
+        computeDiscount(name, gpa);
+
+
+    }
+    public static void computeDiscount(String name, double x){
+
+        System.out.print(name + ", your GPA is " + x + ", so your credit is $" + x * 10 );
+    }
+}
