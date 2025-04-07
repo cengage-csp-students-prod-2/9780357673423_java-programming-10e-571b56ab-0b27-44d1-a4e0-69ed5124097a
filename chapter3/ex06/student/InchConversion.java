9605a7d9c5c7357b7b64d00cc4697523a755f442
Om Mahato <117744764+MahatoOm@users.noreply.github.com>
2025-04-07 15:54:15 +0000

    Testing Mon Apr 07 2025 15:54:15 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex06/student/InchConversion.java
+++ chapter3/ex06/student/InchConversion.java
@@ -1,1 +1,26 @@
 // Write your code here
+
+import java.util.Scanner;
+public class InchConversion{
+    
+    public static void main(String[] args){
+
+        Scanner san = new Scanner(System.in);
+        System.out.print("Enter inches >> ");
+        float inches = san.nextFloat();
+
+        convertToFeet(inches);
+        convertToYards(inches);
+    }
+
+    public static void convertToFeet(float inches){
+
+        System.out.println(inches + " inches is "+ inches / 12 + " feet");
+    }
+
+    public static void convertToYards(float inches){
+
+        System.out.println(inches + " inches is "+ inches / 36 + " yards");
+    }
+
+}
