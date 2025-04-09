171abbb1c36cc220a26cadda9c1233bda6f71cf1
Om Mahato <117744764+MahatoOm@users.noreply.github.com>
2025-04-09 16:19:30 +0000

    Testing Wed Apr 09 2025 16:19:30 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex08/student/Insurance.java
+++ chapter3/ex08/student/Insurance.java
@@ -1,1 +1,27 @@
 // Write your code here
+
+import java.util.Scanner;
+
+public class Insurance{
+
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the current year >> ");
+        int current_year = input.nextInt();
+
+        System.out.print("Enter the birth year >> ");
+        int birth_year = input.nextInt();
+
+        int amount = calculatePremium(current_year, birth_year);
+        System.out.print("The premium is $" + amount);
+    }
+
+    public static int calculatePremium(int cYear, int bYear){
+
+        int decade = (cYear - bYear)/10;
+
+        return (decade + 15 ) * 20;
+
+    }
+}
