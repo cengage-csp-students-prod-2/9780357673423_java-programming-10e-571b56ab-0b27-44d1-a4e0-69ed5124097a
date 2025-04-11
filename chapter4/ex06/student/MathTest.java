225fb79b4da8405f1c718673881e2bf9ea4d5d65
Om Mahato <117744764+MahatoOm@users.noreply.github.com>
2025-04-11 00:48:16 +0000

    Testing Fri Apr 11 2025 00:48:16 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex06/student/MathTest.java
+++ chapter4/ex06/student/MathTest.java
@@ -8,7 +8,7 @@ public class MathTest{
         int num = input.nextInt();
         double val = input.nextDouble();
 
-        System.out.println("THe sqaure root of 37: " + Math.sqrt(37));
+        System.out.println(Math.sqrt(num));
 
         System.out.println("The sine and cosine of 300: " + Math.sin(300) + Math.cos(300));
         System.out.println("The floor, ceiling and rounded value of " + val + " : " + Math.floor(val) + Math.ceil(val) + Math.round(val));
