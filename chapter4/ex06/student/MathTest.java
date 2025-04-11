3fd448d5badc58702c942a2951f2fa556c1a87dd
Om Mahato <117744764+MahatoOm@users.noreply.github.com>
2025-04-11 00:21:38 +0000

    Testing Fri Apr 11 2025 00:21:38 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex06/student/MathTest.java
+++ chapter4/ex06/student/MathTest.java
@@ -1,1 +1,28 @@
 // Write your code here
+// import Math;
+import java.util.Scanner;
+public class MathTest{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        int num = input.nextInt();
+        double val = input.nextDouble();
+
+        System.out.println("THe sqaure root of 37: " + Math.sqrt(37));
+
+        System.out.println("The sine and cosine of 300: " + Math.sin(300) + Math.cos(300));
+        System.out.println("The floor, ceiling and rounded value of " + val + " : " + Math.floor(val) + Math.ceil(val) + Math.round(val));
+
+
+
+    }
+}
+
+// public class Math{
+//     public squareroot(int val){
+//         return (val * val);
+//     }
+
+//     public trignometric(val)
+    
+// }
