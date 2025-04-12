94c61002144cf8c50b5f4dbdf539d9c8a6a60ef3
Om Mahato <117744764+MahatoOm@users.noreply.github.com>
2025-04-12 18:32:56 +0000

    Testing Sat Apr 12 2025 18:32:56 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex09/student/DaysTilNextMonth.java
+++ chapter4/ex09/student/DaysTilNextMonth.java
@@ -1,1 +1,39 @@
 // Write your code here
+
+import java.time.LocalDate;
+import java.util.Scanner;
+class Local_Data{
+    public static int lengthOfMonth(LocalDate data){
+        int day = data.getDayOfMonth();
+        int daystillnextmonth = data.lengthOfMonth() - day;
+        return daystillnextmonth;
+    }
+}
+
+public class DaysTilNextMonth{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the month as an integer >> ");
+        int month = input.nextInt();
+
+        System.out.print("Enter the day >> ");
+        int day = input.nextInt();
+
+        System.out.print("Enter the year using four digits >> ");
+        int year = input.nextInt();
+
+        Local_Data ld = new Local_Data();
+        LocalDate data ;
+        if (month == 12){
+            data = LocalDate.of(year + 1, 1, day);
+        }
+        else{
+            data = LocalDate.of(year, month+1, day);
+        };
+
+        int remainDay = ld.lengthOfMonth(data);
+
+        System.out.print("There are " + remainDay + " days until "+ data.getMonth() + " starts." );
+        
+    }
+}
