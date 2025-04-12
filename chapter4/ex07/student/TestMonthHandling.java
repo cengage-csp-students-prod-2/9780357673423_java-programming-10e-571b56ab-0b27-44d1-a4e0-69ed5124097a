de8534f81b4cd65d8a97daad336a00fc18975def
Om Mahato <117744764+MahatoOm@users.noreply.github.com>
2025-04-12 16:18:09 +0000

    Testing Sat Apr 12 2025 16:18:09 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex07/student/TestMonthHandling.java
+++ chapter4/ex07/student/TestMonthHandling.java
@@ -1,36 +1,38 @@
-// Write your code here
-// import java.LocalDate;
 import java.time.LocalDate;
 import java.util.Scanner;
-public class TestMonthHandling{
-    public static void main(String[] args){
-        
-        LocalDate date1 = getDate();  
-        LocalDate date2 = getDate();
-        
-        DisplayDate(date1);
-        DisplayDate(date2);
 
-    }
-    public static LocalDate getDate(){
+public class TestMonthHandling {
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int yr, mo, da;
-        System.out.print("Enter month >> " );
-        mo = input.nextInt();
-        System.out.print("Enter day >> " );
-        da = input.nextInt();
-        System.out.print("Enter year >> " );
-        yr = input.nextInt();
-        LocalDate data = LocalDate.of(yr, mo, da);
-        return data;
+
+        // Get two dates from user input
+        LocalDate date1 = getDate(input);
+        LocalDate date2 = getDate(input);
+
+        // Display results for each date
+        displayDate(date1);
+        displayDate(date2);
+
+        input.close();
     }
 
-    public static void DisplayDate(LocalDate date){
+    // Method to get a date from the user
+    public static LocalDate getDate(Scanner input) {
+        System.out.print("Enter month >> ");
+        int month = input.nextInt();
+        System.out.print("Enter day >> ");
+        int day = input.nextInt();
+        System.out.print("Enter year >> ");
+        int year = input.nextInt();
 
+        return LocalDate.of(year, month, day);
+    }
+
+    // Method to display the original date and the next three months
+    public static void displayDate(LocalDate date) {
         System.out.println("First date " + date);
-        System.out.println("Second date " +  date.plusMonths(1) );
-        System.out.println("Third date " + date.plusMonths(2) );
-        System.out.println("Fourth date " +  date.plusMonths(3) );
-
+        System.out.println("Second date " + date.plusMonths(1));
+        System.out.println("Third date " + date.plusMonths(2));
+        System.out.println("Fourth date " + date.plusMonths(3));
     }
 }
