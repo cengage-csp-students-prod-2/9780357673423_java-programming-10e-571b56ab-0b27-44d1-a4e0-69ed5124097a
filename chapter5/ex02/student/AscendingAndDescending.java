15bb01f532bc51d976646c13918886e648575539
Om Mahato <117744764+MahatoOm@users.noreply.github.com>
2025-04-13 19:50:41 +0000

    Testing Sun Apr 13 2025 19:50:41 GMT+0000 (Coordinated Universal Time)

--- chapter5/ex02/student/AscendingAndDescending.java
+++ chapter5/ex02/student/AscendingAndDescending.java
@@ -14,39 +14,38 @@ public class AscendingAndDescending{
 
         
 
-        if (a < b) 
 
 
         if (a < b) {
             if (b < c){ 
-                System.out.println("Ascending: " + a + b + c);
-                System.out.println("descending: " + c+ b+ a);
+                System.out.println("Ascending: " + a+ " " + b + " " + c);
+                System.out.println("descending: " + c + " " + b + " " + a);
                    
             }
             else if (b > c){ 
-                System.out.println("Ascending: " + a +  c+ b);
-                System.out.println("descending: " + b + c+ a);
+                System.out.println("Ascending: " + a + " " +  c + " " + b);
+                System.out.println("descending: " + b + " " + c + " " + a);
                    
             }
         }
         else if (b< a){
             if (a<c){
-                System.out.println("Ascending: " +  b + a + c);
-                System.out.println("descending: " + c + a + b);
+                System.out.println("Ascending: " +  b + " " + a + " " + c);
+                System.out.println("descending: " + c + " " + a + " " + b);
             }
             else if (c < a){
-                System.out.println("Ascending: " +  b + c + a);
-                System.out.println("descending: " + a + c + b);
+                System.out.println("Ascending: " +  b + " " + c + " " + a);
+                System.out.println("descending: " + a + " " + c + " " + b);
             }
         }
         else if (c < a ){
             if (a<b){
-                System.out.println("Ascending: " +  c + a + b);
-                System.out.println("descending: " + b + a + c);
+                System.out.println("Ascending: " +  c + " " + a + " " + b);
+                System.out.println("descending: " + b + " " + a + " "+ c);
             }
             else if (b < a){
-                System.out.println("Ascending: " +  c + b + a);
-                System.out.println("descending: " + a + b + c);
+                System.out.println("Ascending: " +  c+ " " + b + " " + a);
+                System.out.println("descending: " + a + " " + b + " " + c);
             }
         }
 
