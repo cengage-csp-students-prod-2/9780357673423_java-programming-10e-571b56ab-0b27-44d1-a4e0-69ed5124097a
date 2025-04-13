0d873af48230875910e61b80ab06301ed6efb03b
Om Mahato <117744764+MahatoOm@users.noreply.github.com>
2025-04-13 23:34:14 +0000

    Testing Sun Apr 13 2025 23:34:14 GMT+0000 (Coordinated Universal Time)

--- chapter5/ex08/student/Automobile.java
+++ chapter5/ex08/student/Automobile.java
@@ -1,1 +1,69 @@
-// Write your code here
+public class Automobile{
+public Automobile(int id, int year, double miles, String make, String model, String color){
+        setId(id);
+        setYear(year);
+        setMiles(miles);
+        setMake(make);
+        setModel(model);
+        setColor(color);
+}
+
+public void setId(int id){
+    final int Max_Id = 9999;
+    if (id < 0 || id > 9999){
+        this.id  = 0;
+    }
+    else{
+        this.id = id; 
+    }
+}
+
+public void setMake(String make){
+    this.make = make;
+}
+
+public void setYear(int year){
+    if( year < 2004 || year > 2024){
+        this.year = 0;
+    }
+    else{
+        this.year = year;
+    }
+}
+
+public void setMiles(double miles){
+    if (miles < 10 || miles > 60){
+        this.miles = 0;
+    }
+    else{
+        this.miles = miles;
+    }
+}
+
+public void setModel(String model){
+    this.model = model;
+}
+
+public void setColor(String color){
+    this.model = model;
+}
+
+public int getId(){
+    return id;
+}
+public double getMiles(){
+    return Miles;
+}
+public int getYear(){
+    return Year;
+}
+public String getColor(){
+    return Color;
+}
+public String getModel(){
+    return Model;
+}
+public String getMake(){
+    return Make;
+}
+}
--- chapter5/ex08/student/TestAutomobiles.java
+++ chapter5/ex08/student/TestAutomobiles.java
@@ -1,1 +1,30 @@
 // Write your code here
+import java.util.Scanner;
+public class TestAutomobiles{
+    private Automobile object1, object2;
+
+    public static void main(String[] args){
+
+        int id, year;
+        double miles;
+        String make, model, color;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter ID (an integer) >> ");
+        id = input.nextInt();
+
+        System.out.print("Enter make >> ");
+        make = input.nextLine() ;
+        System.out.print("Enter model >> ");
+        model = input.nextLine();
+        System.out.print("Enter color >> ");
+        color = input.nextLine();
+
+        System.out.print("Enter year (4 digits) >> ");
+        year = input.nextInt();
+        System.out.print("Enter mpg >> ");
+        miles = input.nextDouble();
+        Automobile auto = new Automobile(id, year, miles, make,model,color);
+        System.out.println("ID#" + auto.getId()+ " " + auto.getMake()+ " " + auto.getModel()+ " " + auto.getColor() + " "+ auto.getYear() + " " + auto.getMiles()+   " miles per gallon" );
+    }
+
+}
