d4ba3bb4993b5c0c76908bf20e632dec83a97296
Om Mahato <117744764+MahatoOm@users.noreply.github.com>
2025-04-21 15:42:30 +0000

    Testing Mon Apr 21 2025 15:42:30 GMT+0000 (Coordinated Universal Time)

--- chapter6/ex05/student/Perfect.java
+++ chapter6/ex05/student/Perfect.java
@@ -1,1 +1,26 @@
 // Write your code here
+import java.util.Scanner;
+public class Perfect{
+    public static void main(String[] args) {
+        // int start = 2;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a maximum number >> ");
+        int end = input.nextInt();
+        int total = 0;
+        for (int start = 1; start<= end; start ++ ){
+            
+            for (int st = 1 ; st < start; st ++){
+            if ( start % st == 0){
+                total += st;
+            }
+        }
+        if (total == start){
+        System.out.println("The number " + total + " is perfect");
+        }
+        total = 0;
+        }
+
+
+
+    }
+}
