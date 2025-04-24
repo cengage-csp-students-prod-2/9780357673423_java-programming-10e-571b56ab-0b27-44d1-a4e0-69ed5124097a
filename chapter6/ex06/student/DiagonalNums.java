6b94419617446184025e8b8e5846c35ad625591d
Om Mahato <117744764+MahatoOm@users.noreply.github.com>
2025-04-24 22:02:32 +0000

    Testing Thu Apr 24 2025 22:02:32 GMT+0000 (Coordinated Universal Time)

--- chapter6/ex06/student/DiagonalNums.java
+++ chapter6/ex06/student/DiagonalNums.java
@@ -1,1 +1,16 @@
 // Write your code here
+import java.util.Scanner;
+public class DiagonalNums{
+    public static void main(String[] args){
+        Scanner input = new Scanner;
+
+        System.out.print("Enter a single dight >> ");
+        int digit = input.nextInt();
+        String space = "";
+
+        for (int i = 0; i< 10; i++){
+            space = space + " ";
+            System.out.println(space + digit);
+        }
+     }
+}
