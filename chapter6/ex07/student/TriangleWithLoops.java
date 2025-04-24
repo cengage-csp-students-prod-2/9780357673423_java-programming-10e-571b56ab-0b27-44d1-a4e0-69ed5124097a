ff28a3e8a397c9d12f091ead33014851f88ab70e
Om Mahato <117744764+MahatoOm@users.noreply.github.com>
2025-04-24 22:31:09 +0000

    Testing Thu Apr 24 2025 22:31:09 GMT+0000 (Coordinated Universal Time)

--- chapter6/ex07/student/TriangleWithLoops.java
+++ chapter6/ex07/student/TriangleWithLoops.java
@@ -1,1 +1,26 @@
 // Write your code here
+
+
+import java.util.Scanner;
+public class TriangleWithLoops{
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number >> ");
+        int digit = input.nextInt();
+        int a = 1;
+        for (int i = 7; i >= 0 ; i--){
+            for (int j = 0; j < i; j++ ){
+                System.out.print(" ");
+            }
+            for (int k = 0; k < a ; k++ ){
+                System.out.print(digit);
+            }
+
+            System.out.print("\n");
+
+            a = a + 2;
+        }
+    }
+}
