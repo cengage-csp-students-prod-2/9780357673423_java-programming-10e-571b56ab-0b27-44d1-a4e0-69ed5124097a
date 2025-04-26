99a66411e1ea0510602935e7b7ad0141ce4a4783
Om Mahato <117744764+MahatoOm@users.noreply.github.com>
2025-04-26 21:49:16 +0000

    Testing Sat Apr 26 2025 21:49:16 GMT+0000 (Coordinated Universal Time)

--- chapter6/ex09/student/IncreasedProduction.java
+++ chapter6/ex09/student/IncreasedProduction.java
@@ -1,1 +1,28 @@
 // Write your code here
+import java.util.Scanner;
+public class IncreasedProduction{
+    public static void main(String[] args){
+
+        Scanner  input = new Scanner(System.in);
+
+        System.out.print("Enter worker's current production level >> ");
+        double prod = input.nextDouble();
+        int res = 0;
+        for (int i = 1; i<=24 ; i++){
+            prod = (prod  + prod * 0.06);
+            System.out.println("Month " + i + "     Predicted production is "+  prod);
+            if (prod >= 10000) {
+                res = i;
+            }
+        }
+        if (prod >= 10000) {
+            System.out.println("The month in which production exceeds 10000.0 is month " + res );
+        }
+        else{
+            System.out.println("This worker will not reach 10000.0 in 24 months");
+
+        }
+        
+        input.close();
+    }
+}
