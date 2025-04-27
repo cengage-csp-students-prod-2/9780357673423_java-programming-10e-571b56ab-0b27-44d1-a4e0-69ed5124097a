c5c431606526e13e10007d79dc81ed29d627fde5
Om Mahato <117744764+MahatoOm@users.noreply.github.com>
2025-04-27 19:07:31 +0000

    Testing Sun Apr 27 2025 19:07:31 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex10/student/PhoneNumberFormat.java
+++ chapter7/ex10/student/PhoneNumberFormat.java
@@ -1,24 +1,25 @@
-// Write your code here
 import java.util.Scanner;
-public class PhoneNumberFormat{
-    public static void main(String[] args){
-
+public class PhoneNumberFormat {
+    public static void main(String[] args) {
         Scanner key = new Scanner(System.in);
-        System.out.print("Enter an area code and phone number as a series of 10 digits Enter 999 to quit >> ");
-
-
+        System.out.print("Enter an area code and phone number as a series of 10 digits\nEnter 999 to quit >> ");
+        
         String phone = key.nextLine();
 
-        while ( phone != "999" ){
-            if (phone.length() == 10){
-            System.out.println("The number is (" + phone.substring(0,3) + ") " + phone.substring(3,6) + "-" + phone.substring(6) );
-            }
-            else{
+        while (!phone.equals("999")) {
+            if (phone.length() == 10) {
+                System.out.println("The number is (" + phone.substring(0,3) + ") " + phone.substring(3,6) + "-" + phone.substring(6));
+            } else {
                 System.out.println("The number is invalid");
             }
-            System.out.print("Enter an area code and phone number as a series of 10 digits and I will display it in a nice format Enter 999 to quit >> ");
-            phone = key.nextLine();
+            System.out.print("\nEnter an area code and phone number as a series of 10 digits\nand I will display it in a nice format\nEnter 999 to quit >> ");
+            
+            if (key.hasNextLine()) {
+                phone = key.nextLine();
+            } else {
+                break; // End of input, exit the loop
+            }
         }
-
+        key.close();
     }
 }
