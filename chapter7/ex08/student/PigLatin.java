e51c8d41c4f883bd7c428df8c872360ed6534818
Om Mahato <117744764+MahatoOm@users.noreply.github.com>
2025-04-27 18:24:59 +0000

    Testing Sun Apr 27 2025 18:24:59 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex08/student/PigLatin.java
+++ chapter7/ex08/student/PigLatin.java
@@ -1,1 +1,34 @@
 // Write your code here
+import java.util.Scanner;
+public class PigLatin{
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a word to be converted to Pig Latin >> ");
+        String word = input.nextLine();
+
+        char start =  word.charAt(0);
+
+        
+        if ((start == 'a') || (start == 'e') || (start == 'i') || (start == 'o') || (start == 'u')){
+            System.out.print("Pig Latin version: " + (word + "ay"));
+        }
+        else{
+            int i = 1;
+            start = word.charAt(i);
+            while (!((start == 'a') || (start == 'e') || (start == 'i') || (start == 'o') || (start == 'u') || (start == 'y'))){
+                i++;
+                start = word.charAt(i);
+            }
+
+            System.out.print("Pig Latin version: " + (word.substring(i) + "ay"));
+
+        }
+
+
+
+        input.close();
+
+    } 
+}
