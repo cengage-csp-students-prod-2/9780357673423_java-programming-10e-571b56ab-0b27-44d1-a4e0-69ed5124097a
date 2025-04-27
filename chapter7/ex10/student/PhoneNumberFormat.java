43b8a52065bdeb52c776c80bad84dee96fe7deba
Om Mahato <117744764+MahatoOm@users.noreply.github.com>
2025-04-27 18:58:23 +0000

    Testing Sun Apr 27 2025 18:58:23 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex10/student/PhoneNumberFormat.java
+++ chapter7/ex10/student/PhoneNumberFormat.java
@@ -11,7 +11,7 @@ public class PhoneNumberFormat{
 
         while ( phone != "999" ){
             if (phone.length() == 10){
-            System.out.println("The number is (" + phone.substring(0,3) + ") " + phone.substring(3,3) + "-" + phone.substring(6) );
+            System.out.println("The number is (" + phone.substring(0,3) + ") " + phone.substring(3,6) + "-" + phone.substring(6) );
             }
             else{
                 System.out.println("The number is invalid");
