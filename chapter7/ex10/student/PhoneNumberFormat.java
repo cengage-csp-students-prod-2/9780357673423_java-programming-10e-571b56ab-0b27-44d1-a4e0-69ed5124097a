41f503b67eca356a0c67680a7cdaf8f1fcf40c32
Om Mahato <117744764+MahatoOm@users.noreply.github.com>
2025-04-27 18:54:44 +0000

    Testing Sun Apr 27 2025 18:54:44 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex10/student/PhoneNumberFormat.java
+++ chapter7/ex10/student/PhoneNumberFormat.java
@@ -1,1 +1,24 @@
 // Write your code here
+import java.util.Scanner;
+public class PhoneNumberFormat{
+    public static void main(String[] args){
+
+        Scanner key = new Scanner(System.in);
+        System.out.print("Enter an area code and phone number as a series of 10 digits Enter 999 to quit >> ");
+
+
+        String phone = key.nextLine();
+
+        while ( phone != "999" ){
+            if (phone.length() == 10){
+            System.out.println("The number is (" + phone.substring(0,3) + ") " + phone.substring(3,3) + "-" + phone.substring(6) );
+            }
+            else{
+                System.out.println("The number is invalid");
+            }
+            System.out.print("Enter an area code and phone number as a series of 10 digits and I will display it in a nice format Enter 999 to quit >> ");
+            phone = key.nextLine();
+        }
+
+    }
+}
