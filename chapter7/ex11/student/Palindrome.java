674811406be3c6837b0eef7307a3dc8921768290
Om Mahato <117744764+MahatoOm@users.noreply.github.com>
2025-04-27 20:15:10 +0000

    Testing Sun Apr 27 2025 20:15:10 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex11/student/Palindrome.java
+++ chapter7/ex11/student/Palindrome.java
@@ -1,1 +1,27 @@
 // Write your code here
+import java.util.Scanner;
+public class Palindrome{
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a string >> ");
+        String word = input.nextLine();
+
+        int i = 0;
+        int j = word.length() - 1 ;
+
+        while (i <= j) {
+
+            if (word.charAt(i) != word.charAt(j)) {
+                System.out.print("No, you did not enter a palindrome");
+                break;
+            }
+
+        }
+        System.out.print("Yes, entered a palindrome");
+
+
+
+
+    }
+}
