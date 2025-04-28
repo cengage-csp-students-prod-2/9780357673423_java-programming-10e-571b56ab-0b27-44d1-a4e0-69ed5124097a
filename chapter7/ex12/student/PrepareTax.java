aaa8e3148d57a042a9eb1d5a32077b0ba0648ca7
Om Mahato <117744764+MahatoOm@users.noreply.github.com>
2025-04-28 06:32:53 +0000

    Testing Mon Apr 28 2025 06:32:53 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex12/student/PrepareTax.java
+++ chapter7/ex12/student/PrepareTax.java
@@ -1,1 +1,52 @@
-// Write your code here
+import java.util.Scanner;
+
+public class PrepareTax {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String ssn;
+        do {
+            System.out.print("Enter your Social Security number >> ");
+            ssn = input.nextLine();
+        } while (!ssn.matches("\\d{3}-\\d{2}-\\d{4}"));
+
+        System.out.print("Enter your first name >> ");
+        String fname = input.nextLine();
+
+        System.out.print("Enter your last name >> ");
+        String lname = input.nextLine();
+
+        System.out.print("Enter your address >> ");
+        String address = input.nextLine();
+
+        System.out.print("Enter your city >> ");
+        String city = input.nextLine();
+
+        System.out.print("Enter your state >> ");
+        String state = input.nextLine();
+
+        String zip;
+        do {
+            System.out.print("Enter your zip code >> ");
+            zip = input.nextLine();
+        } while (!zip.matches("\\d{5}"));
+
+        String maritalStatus;
+        do {
+            System.out.print("Enter marital status >> ");
+            maritalStatus = input.nextLine();
+        } while (!(maritalStatus.startsWith("S") || maritalStatus.startsWith("s") || maritalStatus.startsWith("M") || maritalStatus.startsWith("m")));
+
+        double income;
+        do {
+            System.out.print("Enter your annual income >> ");
+            income = input.nextDouble();
+        } while (income < 0);
+
+        TaxReturn taxReturn = new TaxReturn(ssn, fname, lname, address, city, state, zip, income, maritalStatus);
+
+        taxReturn.display();
+
+        input.close();
+    }
+}
--- chapter7/ex12/student/TaxReturn.java
+++ chapter7/ex12/student/TaxReturn.java
@@ -1,1 +1,56 @@
-// Write your code here
+public class TaxReturn {
+    private String social;
+    private String fname;
+    private String lname;
+    private String street;
+    private String city;
+    private String state;
+    private String zip;
+    private double income;
+    private String marital;
+    private double tax;
+
+    // Correct constructor
+    public TaxReturn(String social, String fname, String lname, String street, String city, String state, String zip, double income, String marital) {
+        this.social = social;
+        this.fname = fname;
+        this.lname = lname;
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.income = income;
+        this.marital = marital;
+
+        calculateTax();
+    }
+
+    private void calculateTax() {
+        char status = Character.toUpperCase(marital.charAt(0)); // S or M
+        if (status == 'S') {
+            if (income <= 20000)
+                tax = income * 0.15;
+            else if (income <= 50000)
+                tax = income * 0.22;
+            else
+                tax = income * 0.30;
+        } else if (status == 'M') {
+            if (income <= 20000)
+                tax = income * 0.14;
+            else if (income <= 50000)
+                tax = income * 0.20;
+            else
+                tax = income * 0.28;
+        } else {
+            tax = 0; // Unknown marital status
+        }
+    }
+
+    public void display() {
+        System.out.println("Taxpayer ssn: " + social + " " + fname + " " + lname);
+        System.out.println(street);
+        System.out.println(city + ", " + state + " " + zip);
+        System.out.println("Marital status: " + marital.charAt(0));
+        System.out.println("Income: $" + income + "  Tax: $" + tax);
+    }
+}
