b46f36d2597dede915c39db2fa590dd4b2ec3c22
Om Mahato <117744764+MahatoOm@users.noreply.github.com>
2025-04-30 15:12:53 +0000

    Testing Wed Apr 30 2025 15:12:53 GMT+0000 (Coordinated Universal Time)

--- chapter8/ex05/student/DemoSalesperson.java
+++ chapter8/ex05/student/DemoSalesperson.java
@@ -1,1 +1,15 @@
 // Write your code here
+
+public class DemoSalesperson{
+    public static void main(String[] args){
+        for (int i = 0 ; i < 11 ; i ++){
+            Salesperson sp = new Salesperson(9999, 0.0);
+
+            
+
+            System.out.println("Salesperson " + i + " has ID #" + sp.getId() + "and annual sales of $" + sp.getSales());
+
+        }
+
+}
+}
--- chapter8/ex05/student/Salesperson.java
+++ chapter8/ex05/student/Salesperson.java
@@ -1,1 +1,31 @@
 // Write your code here
+public class Salesperson{
+    int id ;
+    double sales;
+
+    public Salesperson(){
+        this.id = 0;
+        this.sales = 0;
+    }
+
+    public Salesperson(int id, double sales){
+        setId(id);
+        setSales(sales);
+    }
+
+    public int getId(){
+        return this.id;
+    }
+
+    public double getSales(){
+        return this.sales;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+    public void setSales(double sales){
+        this.sales = sales;
+    }
+    
+} 
