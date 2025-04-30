2b3e6b38165cf40925deabece7fb1e224f66d12f
Om Mahato <117744764+MahatoOm@users.noreply.github.com>
2025-04-30 15:17:58 +0000

    Testing Wed Apr 30 2025 15:17:58 GMT+0000 (Coordinated Universal Time)

--- chapter8/ex05/student/DemoSalesperson2.java
+++ chapter8/ex05/student/DemoSalesperson2.java
@@ -1,1 +1,17 @@
 // Write your code here
+
+// Write your code here
+
+public class DemoSalesperson2{
+    public static void main(String[] args){
+        for (int i = 0 ; i < 10 ; i ++){
+            Salesperson sp = new Salesperson(111 + i, 25000 + i * 5000);
+
+            
+
+            System.out.println("Salesperson " + i + " has ID #" + sp.getId() + "and annual sales of $" + sp.getSales());
+
+        }
+
+}
+}
