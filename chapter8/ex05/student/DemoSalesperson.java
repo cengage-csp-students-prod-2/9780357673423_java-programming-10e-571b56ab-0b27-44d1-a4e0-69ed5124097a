19e30b5b57375ff0ee8b416a635f008ab0f01832
Om Mahato <117744764+MahatoOm@users.noreply.github.com>
2025-04-30 15:14:51 +0000

    Testing Wed Apr 30 2025 15:14:51 GMT+0000 (Coordinated Universal Time)

--- chapter8/ex05/student/DemoSalesperson.java
+++ chapter8/ex05/student/DemoSalesperson.java
@@ -2,7 +2,7 @@
 
 public class DemoSalesperson{
     public static void main(String[] args){
-        for (int i = 0 ; i < 11 ; i ++){
+        for (int i = 0 ; i < 10 ; i ++){
             Salesperson sp = new Salesperson(9999, 0.0);
 
             
