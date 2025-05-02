d652f178c02dd61a52ad2e67a4da6feac4413298
Om Mahato <117744764+MahatoOm@users.noreply.github.com>
2025-05-02 16:45:56 +0000

    Testing Fri May 02 2025 16:45:56 GMT+0000 (Coordinated Universal Time)

--- chapter8/ex05/student/SalespersonDatabase.java
+++ chapter8/ex05/student/SalespersonDatabase.java
@@ -5,9 +5,11 @@ public class SalespersonDatabase{
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         Salesperson arr[] = new Salesperson[20];
+        String ch;
 
         do{
         System.out.print("Do you want to (A)dd, (D)elete, or (C)hange a record or (Q)uit >> ");
+        ch = nextLine();
 
         if (ch.equalsIgnoreCase("A")){
             System.out.print("Enter salesperson ID >> ");
@@ -24,15 +26,32 @@ public class SalespersonDatabase{
 
         }
         else if (ch.equalsIgnoreCase("D")){
+            System.out.print("Enter ID to change >> ");
+            int id = input.nextInt();
+            
+            
+
+            System.out.println("Current database:");
+            for (int i = 0; i < arr.length ; i ++){
+                if (arr[i].getId() == id ){
+                    continue;
+                }
+                System.out.println("ID #" + arr[i].getId() + " $" + arr[i].getSales());
+            }
+
+        }
+
+        else if (ch.equalsIgnoreCase("C")){
             System.out.print("Enter salesperson ID >> ");
             int id = input.nextInt();
             System.out.print("Enter sales >> ");
             double sales = input.nextDouble();
-            int l = arr.length;
-            arr[l+1] = new Salesperson(id,sales);
-
+            
             System.out.println("Current database:");
             for (int i = 0; i < arr.length ; i ++){
+                if (arr[i].getId() == id ){
+                    arr[i].setSales(sales);
+                }
                 System.out.println("ID #" + arr[i].getId() + " $" + arr[i].getSales());
             }
 
