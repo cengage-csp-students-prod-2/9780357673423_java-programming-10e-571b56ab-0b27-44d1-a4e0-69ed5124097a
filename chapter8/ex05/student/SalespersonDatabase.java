19002762819303f8cafb39a6da718b83fe1e873d
Om Mahato <117744764+MahatoOm@users.noreply.github.com>
2025-05-03 08:29:53 +0000

    Testing Sat May 03 2025 08:29:53 GMT+0000 (Coordinated Universal Time)

--- chapter8/ex05/student/SalespersonDatabase.java
+++ chapter8/ex05/student/SalespersonDatabase.java
@@ -4,61 +4,125 @@ public class SalespersonDatabase{
 
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        Salesperson arr[] = new Salesperson[20];
+        Salesperson[] arr = new Salesperson[20];
         String ch;
+        int count = 0;
 
         do{
         System.out.print("Do you want to (A)dd, (D)elete, or (C)hange a record or (Q)uit >> ");
-        ch = nextLine();
+        ch = input.nextLine();
+
 
         if (ch.equalsIgnoreCase("A")){
-            System.out.print("Enter salesperson ID >> ");
-            int id = input.nextInt();
-            System.out.print("Enter sales >> ");
-            double sales = input.nextDouble();
-            int l = arr.length;
-            arr[l+1] = new Salesperson(id,sales);
-
-            System.out.println("Current database:");
-            for (int i = 0; i < arr.length ; i ++){
-                System.out.println("ID #" + arr[i].getId() + " $" + arr[i].getSales());
+            if (count >= 20){
+                System.out.print("Array limit exceeded");
+                continue;
             }
 
+            System.out.print("Enter salesperson ID >> ");
+            int id = input.nextInt();
+            boolean exist = false;
+
+            for (int i = 0; i < count; i++){
+                if (arr[i].getId() == id){
+                    System.out.print("ID already exist.");
+                    exist = true;
+                    break;
+                }
+                count++;          
         }
+        if (!exist){
+            System.out.print("Enter sales >> ");
+            double sales = input.nextDouble();
+            arr[count] = new Salesperson(id,sales);
+            count++;
+            
+        }
+        }
+
+
+    
         else if (ch.equalsIgnoreCase("D")){
+            if (count == 0){
+                System.out.print("Array is empty");
+            }
+
             System.out.print("Enter ID to change >> ");
             int id = input.nextInt();
             
-            
+            boolean exist = false;
 
-            System.out.println("Current database:");
-            for (int i = 0; i < arr.length ; i ++){
-                if (arr[i].getId() == id ){
-                    continue;
+            for (int i = 0; i < count; i++){
+                if (arr[i].getId() == id){
+                    exist = true;
+
+                    for (int j = i; j < count; j++){
+                        arr[j] = arr[j+1];
+                    }
+                    arr[count-1] = null;
+                    count--;
+                    break;
                 }
-                System.out.println("ID #" + arr[i].getId() + " $" + arr[i].getSales());
+                
+
             }
+            if (!exist){
+                System.out.print("ID does not exist");
+
+            }
+
 
         }
 
         else if (ch.equalsIgnoreCase("C")){
+            if (count == 0){
+                System.out.print("Array is empty");
+                continue;
+            }
             System.out.print("Enter salesperson ID >> ");
             int id = input.nextInt();
-            System.out.print("Enter sales >> ");
-            double sales = input.nextDouble();
-            
-            System.out.println("Current database:");
-            for (int i = 0; i < arr.length ; i ++){
-                if (arr[i].getId() == id ){
+            boolean exist = false;
+
+            for (int i = 0; i < count; i++){
+                if (arr[i].getId() == id){
+                    System.out.print("Enter sales >> ");
+                    double sales = input.nextDouble();
                     arr[i].setSales(sales);
+                    exist =  true;
+
+                    break;
                 }
-                System.out.println("ID #" + arr[i].getId() + " $" + arr[i].getSales());
+            }
+
+            if (!exist){
+                System.out.print("ID does not exist");
+
             }
 
         }
 
-        
-    } while(ch.equalsIgnoreCase("q")); 
+        for (int i =0 ; i < count; i++){
+            for (int j = i; j < count; j ++){
+                if ( arr[i].getId() > arr[j].getId() ) { 
+                Salesperson sp = arr[i];
+                arr[i] = arr[j] ;
+                arr[j] = sp;
+            }
+        }
+        }
 
+            
+            
+            System.out.println("Current database:");
+            for (int i = 0; i < count ; i ++){
+                System.out.println("ID #" + arr[i].getId() + " $" + arr[i].getSales());
+            }            
+    } while(!ch.equalsIgnoreCase("Q"));
+
+
+
+
+    }
 }
-}
+
+
