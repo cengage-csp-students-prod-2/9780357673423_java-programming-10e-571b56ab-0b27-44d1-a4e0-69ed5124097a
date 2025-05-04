98a467ba4767bb2fdc5fc984b45ab3f71ae6ac50
Om Mahato <117744764+MahatoOm@users.noreply.github.com>
2025-05-04 23:48:57 +0000

    Testing Sun May 04 2025 23:48:57 GMT+0000 (Coordinated Universal Time)

--- chapter8/ex09/student/Purchase.java
+++ chapter8/ex09/student/Purchase.java
@@ -1,1 +1,19 @@
 // Write your code here
+public class Purchase{
+    private int invoiceNumber;
+    private double saleAmount;
+    private double tax;
+
+    public void setSaleAmount(double sa){
+        saleAmount = sa;
+        tax = saleAmount + (0.05 * saleAmount);
+
+    }
+    public void setInvoiceNumber(int inVo){
+        invoiceNumber = inVo;
+    }
+
+    public void Display(){
+        System.out.println("Invoice #" + invoiceNumber + " Amount of sale: $"+ saleAmount +"Tax: $"+ tax );
+    }
+}
--- chapter8/ex09/student/PurchaseArray.java
+++ chapter8/ex09/student/PurchaseArray.java
@@ -1,1 +1,37 @@
 // Write your code here
+import java.util.Scanner;
+public class PurchaseArray{
+    public static void main(String[] main){
+           Purchase[] purch = new Purchase[5];
+           Scanner input = new Scanner(System.in);
+           
+           for (int i = 0; i < 5; i++){
+                purch[i] = new Purchase();
+                System.out.print("Enter invoice number >> ");
+                int invoice = input.nextInt();
+                System.out.print("Enter sale amount >> ");
+                double sale = input.nextDouble();
+
+                while (invoice <1000 && invoice > 8000){
+                    System.out.print("Enter invoice number >> ");
+                    invoice = input.nextInt();
+                }
+                while (sale < 0){
+                    System.out.print("Enter sale amount >> ");
+                    sale = input.nextDouble();
+                }
+
+                purch[i].setInvoiceNumber(invoice);
+                purch[i].setSaleAmount(sale);
+
+           }
+
+        // Now displaying message
+           System.out.println("Summary of purchase:");
+           for (int i=0; i< 5; i++){
+            purch[i].Display();
+            
+           }
+           input.close();
+    }
+}
