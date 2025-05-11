da57d5d1e0a5b06af85c32e4b0453a5aab7546fd
Om Mahato <117744764+MahatoOm@users.noreply.github.com>
2025-05-11 19:58:57 +0000

    Testing Sun May 11 2025 19:58:57 GMT+0000 (Coordinated Universal Time)

--- chapter9/ex12/student/DemoTurners2.java
+++ chapter9/ex12/student/DemoTurners2.java
@@ -1,1 +1,28 @@
-// Write your code here
+import java.util.Scanner;
+public class DemoTurners2{
+
+    public static void main(String[] args){
+
+
+        for (int i = 0 ; i < 2 ; i ++){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Do you want to create a (L)eaf, a (P)ageor a pan(C)ake >> ");
+        char ch = scan.nextLine().charAt(0);
+
+        if (ch == 'L'){
+            Leaf leaf = new Leaf();
+            leaf.turn();
+        }
+        else if (ch == 'P'){
+            Page page = new Page();
+            page.turn();
+        }
+        else if (ch == 'C'){
+            Pancake p = new Pancake();
+            p.turn();
+
+        }
+    }
+    }
+}
