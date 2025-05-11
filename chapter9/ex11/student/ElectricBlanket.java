21573d7b0d1feef7e599d3a00a058e7a971f6bfa
Om Mahato <117744764+MahatoOm@users.noreply.github.com>
2025-05-11 16:56:13 +0000

    Testing Sun May 11 2025 16:56:13 GMT+0000 (Coordinated Universal Time)

--- chapter9/ex11/student/ElectricBlanket.java
+++ chapter9/ex11/student/ElectricBlanket.java
@@ -1,48 +1,44 @@
-// Write your code here
-public class ElectricBlanket extends Blanket{
+public class ElectricBlanket extends Blanket {
     protected int settings;
     protected boolean hasAutoShutoff;
-    
-    
-    public ElectricBlanket(){
+
+    public ElectricBlanket() {
         super();
         settings = 1;
         hasAutoShutoff = false;
-
     }
 
-    public void setSettings(int st){
-        if ((st >= 1) && (st <=5)) {
+    public int getSettings() {
+        return settings;
+    }
+
+    public void setSettings(int st) {
+        if (st >= 1 && st <= 5) {
             settings = st;
-        }
-        else{
+        } else {
             settings = 1;
         }
-        
+    }
+
+    public boolean getHasAutoShutoff() {
+        return hasAutoShutoff;
     }
 
     public void setHasAutoShutoff(boolean shutoff) {
-    if (shutoff && !hasAutoShutoff) {
-        price += 5.75;
-    } else if (!shutoff && hasAutoShutoff) {
-        price -= 5.75;
+        if (shutoff && !hasAutoShutoff) {
+            price += 5.75;
+        } else if (!shutoff && hasAutoShutoff) {
+            price -= 5.75;
+        }
+        hasAutoShutoff = shutoff;
     }
-    hasAutoShutoff = shutoff;
-}
-
-
-    public int getSettings(){
-        return settings;
-    }
-    public boolean getHasAutoShutoff(){
-        return hasAutoShutoff;
-    }
-    
 
     @Override
     public String toString() {
         String base = super.toString();
-        base += hasAutoShutoff ? ". Blanket has automatic shutoff and " : ". no automatic shutoff and ";
+        base += hasAutoShutoff
+            ? ". Blanket has automatic shutoff and "
+            : ". no automatic shutoff and ";
         base += settings + " setting" + (settings != 1 ? "s." : ".");
         return base;
     }
